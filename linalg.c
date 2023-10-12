eb069346b26cbe800d59cd25f2cb3f93cb696506
moosipea <mattiasmcvolt@gmail.com>
2023-10-12 20:12:35 +0300

    removed the shitty namespacing

--- linalg.c
+++ linalg.c
@@ -5,118 +5,111 @@
 * Vec4 functions
 */
 
-struct LINALG(Vec4) LINALG(Vec4_create)(float x, float y, float z, float w) {
-    struct LINALG(Vec4) res;
-    res.x = x;
-    res.y = y;
-    res.z = z;
-    res.w = w;
-    return res;
+struct linalg_Vec4 linalg_Vec4_create(float x, float y, float z, float w) {
+  struct linalg_Vec4 res;
+  res.x = x;
+  res.y = y;
+  res.z = z;
+  res.w = w;
+  return res;
 }
 
-struct LINALG(Vec4) LINALG(Vec4_add)(struct LINALG(Vec4) v0, struct LINALG(Vec4) v1) {
-    struct LINALG(Vec4) res;
-    res.x = v0.x + v1.x;
-    res.y = v0.y + v1.y;
-    res.z = v0.z + v1.z;
-    res.w = v0.w + v1.w;
-    return res;
+struct linalg_Vec4 linalg_Vec4_sub(struct linalg_Vec4 v0,
+                                   struct linalg_Vec4 v1) {
+  struct linalg_Vec4 res;
+  res.x = v0.x - v1.x;
+  res.y = v0.y - v1.y;
+  res.z = v0.z - v1.z;
+  res.w = v0.w - v1.w;
+  return res;
 }
 
-struct LINALG(Vec4) LINALG(Vec4_sub)(struct LINALG(Vec4) v0, struct LINALG(Vec4) v1) {
-    struct LINALG(Vec4) res;
-    res.x = v0.x - v1.x;
-    res.y = v0.y - v1.y;
-    res.z = v0.z - v1.z;
-    res.w = v0.w - v1.w;
-    return res;
+struct linalg_Vec4 linalg_Vec4_mul(struct linalg_Vec4 v, float k) {
+  struct linalg_Vec4 res;
+  res.x = v.x * k;
+  res.y = v.y * k;
+  res.z = v.z * k;
+  res.w = v.w * k;
+  return res;
 }
 
-struct LINALG(Vec4) LINALG(Vec4_mul)(struct LINALG(Vec4) v, float k) {
-    struct LINALG(Vec4) res;
-    res.x = v.x*k;
-    res.y = v.y*k;
-    res.z = v.z*k;
-    res.w = v.w*k;
-    return res;
+struct linalg_Vec4 linalg_Vec4_div(struct linalg_Vec4 v, float k) {
+  struct linalg_Vec4 res;
+  res.x = v.x / k;
+  res.y = v.y / k;
+  res.z = v.z / k;
+  res.w = v.w / k;
+  return res;
 }
 
-struct LINALG(Vec4) LINALG(Vec4_div)(struct LINALG(Vec4) v, float k) {
-    struct LINALG(Vec4) res;
-    res.x = v.x / k;
-    res.y = v.y / k;
-    res.z = v.z / k;
-    res.w = v.w / k;
-    return res;
+float linalg_Vec4_mag(struct linalg_Vec4 v) {
+  return sqrtf(v.x * v.x + v.y * v.y + v.z * v.z + v.w * v.w);
 }
 
-float LINALG(Vec4_mag)(struct LINALG(Vec4) v) {
-    return sqrtf(v.x*v.x + v.y*v.y + v.z*v.z + v.w*v.w);
+struct linalg_Vec4 linalg_Vec4_norm(struct linalg_Vec4 v) {
+  struct linalg_Vec4 res;
+  float mag;
+  res = v;
+  mag = linalg_Vec4_mag(v);
+  return linalg_Vec4_div(res, mag);
 }
 
-struct LINALG(Vec4) LINALG(Vec4_norm)(struct LINALG(Vec4) v) {
-    struct LINALG(Vec4) res;
-    float mag;
-    res = v;
-    mag = LINALG(Vec4_mag)(v);
-    return LINALG(Vec4_div)(res, mag);
-}
-
-float LINALG(Vec4_dot)(struct LINALG(Vec4) v0, struct LINALG(Vec4) v1) {
-    return v0.x*v1.x + v0.y*v1.y + v0.z*v1.z + v0.w*v1.w;
+float linalg_Vec4_dot(struct linalg_Vec4 v0, struct linalg_Vec4 v1) {
+  return v0.x * v1.x + v0.y * v1.y + v0.z * v1.z + v0.w * v1.w;
 }
 
 /*
 * Mat4x4 functions
 */
 
-struct LINALG(Mat4x4) LINALG(Mat4x4_ident)(void) {
-    struct LINALG(Mat4x4) mat = {0};
-    mat.m[0][0] = 1.0f;
-    mat.m[1][1] = 1.0f;
-    mat.m[2][2] = 1.0f;
-    mat.m[3][3] = 1.0f;
-    return mat;
+struct linalg_Mat4x4 linalg_Mat4x4_ident(void) {
+  struct linalg_Mat4x4 mat = {0};
+  mat.m[0][0] = 1.0f;
+  mat.m[1][1] = 1.0f;
+  mat.m[2][2] = 1.0f;
+  mat.m[3][3] = 1.0f;
+  return mat;
 }
 
-void LINALG(Mat4x4_matmul)(struct LINALG(Mat4x4) *with, struct LINALG(Mat4x4) *mat) {
-    for (int y = 0; y < 4; y++) {
-        for (int x = 0; x < 4; x++) {
-            mat->m[y][x] = mat->m[y][0]*with->m[0][x] +
-                           mat->m[y][1]*with->m[1][x] +
-                           mat->m[y][2]*with->m[2][x] +
-                           mat->m[y][3]*with->m[3][x];
-        }
+void linalg_Mat4x4_matmul(struct linalg_Mat4x4 *with,
+                          struct linalg_Mat4x4 *mat) {
+  for (int y = 0; y < 4; y++) {
+    for (int x = 0; x < 4; x++) {
+      mat->m[y][x] =
+          mat->m[y][0] * with->m[0][x] + mat->m[y][1] * with->m[1][x] +
+          mat->m[y][2] * with->m[2][x] + mat->m[y][3] * with->m[3][x];
     }
+  }
 }
 
-struct LINALG(Mat4x4) LINALG(Mat4x4_translation)(float dx, float dy, float dz) {
-    struct LINALG(Mat4x4) mat = LINALG(Mat4x4_ident)();
-    mat.m[0][3] = dx;
-    mat.m[1][3] = dx;
-    mat.m[2][3] = dx;
-    return mat;
+struct linalg_Mat4x4 linalg_Mat4x4_translation(float dx, float dy, float dz) {
+  struct linalg_Mat4x4 mat = linalg_Mat4x4_ident();
+  mat.m[0][3] = dx;
+  mat.m[1][3] = dx;
+  mat.m[2][3] = dx;
+  return mat;
 }
 
-struct LINALG(Mat4x4) LINALG(Mat4x4_scaling)(float sx, float sy, float sz) {
-    struct LINALG(Mat4x4) mat = LINALG(Mat4x4_ident)();
-    mat.m[0][0] = sx;
-    mat.m[1][1] = sx;
-    mat.m[2][2] = sx;
-    return mat;
+struct linalg_Mat4x4 linalg_Mat4x4_scaling(float sx, float sy, float sz) {
+  struct linalg_Mat4x4 mat = linalg_Mat4x4_ident();
+  mat.m[0][0] = sx;
+  mat.m[1][1] = sx;
+  mat.m[2][2] = sx;
+  return mat;
 }
 
-struct LINALG(Mat4x4) LINALG(Mat4x4_rotation_x)(float r);
-struct LINALG(Mat4x4) LINALG(Mat4x4_rotation_y)(float r);
-struct LINALG(Mat4x4) LINALG(Mat4x4_rotation_z)(float r);
+struct linalg_Mat4x4 linalg_Mat4x4_rotation_x(float r);
+struct linalg_Mat4x4 linalg_Mat4x4_rotation_y(float r);
+struct linalg_Mat4x4 linalg_Mat4x4_rotation_z(float r);
 
-struct LINALG(Mat4x4) LINALG(Mat4x4_rotation)(float rx, float ry, float rz) {
-    struct LINALG(Mat4x4) mat_z = LINALG(Mat4x4_rotation_z)(rz);
-    struct LINALG(Mat4x4) mat_x = LINALG(Mat4x4_rotation_x)(rx);
-    struct LINALG(Mat4x4) mat_y = LINALG(Mat4x4_rotation_y)(ry);
-    LINALG(Mat4x4_matmul)(&mat_x, &mat_z);
-    LINALG(Mat4x4_matmul)(&mat_y, &mat_z);
-    return mat_z;
+struct linalg_Mat4x4 linalg_Mat4x4_rotation(float rx, float ry, float rz) {
+  struct linalg_Mat4x4 mat_z = linalg_Mat4x4_rotation_z(rz);
+  struct linalg_Mat4x4 mat_x = linalg_Mat4x4_rotation_x(rx);
+  struct linalg_Mat4x4 mat_y = linalg_Mat4x4_rotation_y(ry);
+  linalg_Mat4x4_matmul(&mat_x, &mat_z);
+  linalg_Mat4x4_matmul(&mat_y, &mat_z);
+  return mat_z;
 }
 
-struct LINALG(Mat4x4) LINALG(Mat4x4_perspective)(float fovy, float aspect, float near, float far);
+struct linalg_Mat4x4 linalg_Mat4x4_perspective(float fovy, float aspect,
+                                               float near, float far);
--- linalg.h
+++ linalg.h
@@ -1,48 +1,48 @@
 #ifndef LINALG_H
 #define LINALG_H
 
-#ifndef LINALG_PREFIX
-#define LINALG_PREFIX linalg
-#endif
-
-#define __LINALG(prefix,name) prefix ## _ ## name
-#define _LINALG(prefix,name) __LINALG(prefix,name)
-#define LINALG(name) _LINALG(LINALG_PREFIX,name)
-
 /*
 * Structure definitions
 */
 
-struct LINALG(Vec4)   { float x, y, z, w; };
-struct LINALG(Mat4x4) { float m[4][4]; };
+struct linalg_Vec4 {
+  float x, y, z, w;
+};
+struct linalg_Mat4x4 {
+  float m[4][4];
+};
 
 /*
 * Vec4 functions
 */
 
-struct LINALG(Vec4) LINALG(Vec4_create)(float x, float y, float z, float w);
-struct LINALG(Vec4) LINALG(Vec4_add)(struct LINALG(Vec4) v0, struct LINALG(Vec4) v1);
-struct LINALG(Vec4) LINALG(Vec4_sub)(struct LINALG(Vec4) v0, struct LINALG(Vec4) v1);
-struct LINALG(Vec4) LINALG(Vec4_mul)(struct LINALG(Vec4) v, float k);
-struct LINALG(Vec4) LINALG(Vec4_div)(struct LINALG(Vec4) v, float k);
-struct LINALG(Vec4) LINALG(Vec4_matmul)(struct LINALG(Vec4) v, struct LINALG(Mat4x4) *mat); /* TODO */
-float LINALG(Vec4_mag)(struct LINALG(Vec4) v);
-struct LINALG(Vec4) LINALG(Vec4_norm)(struct LINALG(Vec4) v);
-float LINALG(Vec4_dot)(struct LINALG(Vec4) v0, struct LINALG(Vec4) v1);
+struct linalg_Vec4 linalg_Vec4_create(float x, float y, float z, float w);
+struct linalg_Vec4 linalg_Vec4_add(struct linalg_Vec4 v0,
+                                   struct linalg_Vec4 v1);
+struct linalg_Vec4 linalg_Vec4_sub(struct linalg_Vec4 v0,
+                                   struct linalg_Vec4 v1);
+struct linalg_Vec4 linalg_Vec4_mul(struct linalg_Vec4 v, float k);
+struct linalg_Vec4 linalg_Vec4_div(struct linalg_Vec4 v, float k);
+struct linalg_Vec4 linalg_Vec4_matmul(struct linalg_Vec4 v,
+                                      struct linalg_Mat4x4 *mat); /* TODO */
+float linalg_Vec4_mag(struct linalg_Vec4 v);
+struct linalg_Vec4 linalg_Vec4_norm(struct linalg_Vec4 v);
+float linalg_Vec4_dot(struct linalg_Vec4 v0, struct linalg_Vec4 v1);
 
 /*
 * Mat4x4 functions TODO
 */
 
-struct LINALG(Mat4x4) LINALG(Mat4x4_ident)(void);
-void LINALG(Mat4x4_matmul)(struct LINALG(Mat4x4) *left, struct LINALG(Mat4x4) *right);
-struct LINALG(Mat4x4) LINALG(Mat4x4_translation)(float dx, float dy, float dz);
-struct LINALG(Mat4x4) LINALG(Mat4x4_scaling)(float sx, float sy, float sz);
-struct LINALG(Mat4x4) LINALG(Mat4x4_rotation_x)(float r);
-struct LINALG(Mat4x4) LINALG(Mat4x4_rotation_y)(float r);
-struct LINALG(Mat4x4) LINALG(Mat4x4_rotation_z)(float r);
-struct LINALG(Mat4x4) LINALG(Mat4x4_rotation)(float rx, float ry, float rz);
-struct LINALG(Mat4x4) LINALG(Mat4x4_perspective)(float fovy, float aspect, float near, float far);
-
+struct linalg_Mat4x4 linalg_Mat4x4_ident(void);
+void linalg_Mat4x4_matmul(struct linalg_Mat4x4 *left,
+                          struct linalg_Mat4x4 *right);
+struct linalg_Mat4x4 linalg_Mat4x4_translation(float dx, float dy, float dz);
+struct linalg_Mat4x4 linalg_Mat4x4_scaling(float sx, float sy, float sz);
+struct linalg_Mat4x4 linalg_Mat4x4_rotation_x(float r);
+struct linalg_Mat4x4 linalg_Mat4x4_rotation_y(float r);
+struct linalg_Mat4x4 linalg_Mat4x4_rotation_z(float r);
+struct linalg_Mat4x4 linalg_Mat4x4_rotation(float rx, float ry, float rz);
+struct linalg_Mat4x4 linalg_Mat4x4_perspective(float fovy, float aspect,
+                                               float near, float far);
 
 #endif /* LINALG_H */
