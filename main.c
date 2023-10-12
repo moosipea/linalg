93f54909e020c081640e94388347977286540018
moosipea <mattiasmcvolt@gmail.com>
2023-10-12 23:34:41 +0300

    zzzz

--- main.c
+++ main.c
@@ -1,16 +1,6 @@
 #include <stdio.h>
-
-#define LINALG_INLINE_FUNCTIONS
 #include "linalg.h"
 
 int main(void) {
-#if 0
-    struct linalg_Vec4 a = linalg_Vec4_create(0, 0, 0, 0);
-    linalg_Vec4_add(&a, linalg_Vec4_create(1, 2, 3, 4));
-    linalg_Vec4_sub(&a, linalg_Vec4_create(0, 3, 0, -1));
-
-    printf("Result: { x: %f, y: %f, z: %f, w: %f}\n", a.x, a.y, a.z, a.w);
-
-#endif
     return 0;
 }
